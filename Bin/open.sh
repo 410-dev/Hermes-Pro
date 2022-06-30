e005ec6ffbd866287b21de1913d1c6665e763c54
410 <me@hysong.dev>
2022-07-01 03:23:33 +0900

    Create open.sh

---
+++ Bin/open.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Open application.
+# @param $1 - Application name.
+# @param $@ - Application parameters.
+# @return 0 - Success.
+# @return 1 - Failure.
+
+@import Foundation/out
+println "Command not prepared."
